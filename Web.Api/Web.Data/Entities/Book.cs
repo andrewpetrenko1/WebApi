5fa5265aa531be8218df19fbbab3bb4553f85cd7
Andrew Petrenko <andreypetrenko1998@gmail.com>
2020-10-22 17:27:19 +0300

    Initial commit

---
+++ Web.Api/Web.Data/Entities/Book.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Web.Data.Entities
+{
+	class Book
+	{
+	}
+}
---
+++ Web.Api/Web.Data/WebApiDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Web.Data.Entities;
+
+namespace Web.Data
+{
+	class WebApiDbContext : DbContext
+	{
+		private readonly IConfiguration _configuration;
+		public WebApiDbContext(IConfiguration configuration) : base()
+		{
+			this._configuration = configuration;
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder builder)
+		{
+			builder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			
+		}
+
+		public DbSet<Book> Books { get; set; }
+	}
+}
